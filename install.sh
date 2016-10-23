77d09b43f26639924c8211bd54a0f480cf05fd33
Zach Deibert <zachdeibert@gmail.com>
2016-10-23 12:49:52 -0500

    Even more install.sh changes

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
+set -e
 
 # Install javascript dependencies
-npm Install
+npm install
 
 # Install ruby dependencies
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL http://get.rvm.io | bash -s stable --ruby
-source ~/.rvm/scripts/rvm
 rvm install 2.3.0
 bundle install
