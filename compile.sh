7d9cf9f681ad53e2de358f3085add46cb81ccb7a
Zach Deibert <zachdeibert@gmail.com>
2016-10-23 11:15:08 -0500

    Finished merging in old repo

--- compile.sh
+++ compile.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
+set -e
 
-mkdir -p _site
-cat <<EOF > _site/index.html
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Hello, world!</title>
-	</head>
-	<body>
-		<h1>Hello, world!</h1>
-	</body>
-</html>
-EOF
-cat <<EOF > _site/CNAME
-www.latipium.com
-EOF
+# Transpile typescripts
+tsc --sourceMap || true
+tsc -p electron --sourceMap || true
+
+# Build Jekyll content
+jekyll build
--- old/nuget/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -e "s|{{ *password *}}|$(cat /etc/latipium/website/mysql/nuget_password)|g" \
-    /var/local/simple-nuget-server/inc/config.php.in > /var/local/simple-nuget-server/inc/config.php
-php5-fpm --fpm-config /etc/php5/fpm/php-fpm.conf
