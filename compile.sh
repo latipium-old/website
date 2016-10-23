16cc4b7be8ad8a9cc5a76b94b40b1812bd06a04a
Zach Deibert <zachdeibert@gmail.com>
2016-10-23 13:38:30 -0500

    Added development server option

--- compile.sh
+++ compile.sh
@@ -1,13 +1,42 @@
 #!/bin/bash --login
 set -e
 
+dev=0
+if [ "a$1" == "adev" ]; then
+    dev=1
+fi
+
+function prefixOutput() {
+    while read line; do
+        echo "$1$line"
+    done
+}
+
 # Update the PATH
 rvm use 2.3.0
 PATH=$PATH:node_modules/.bin/:$(bundle show jekyll)/exe/
 
 # Transpile typescripts
-tsc --sourceMap || true
-tsc -p electron --sourceMap || true
+function tsProj() {
+    if [ $dev -eq 1 ]; then
+        tsc -p "$1" --sourceMap -w | prefixOutput "$2" &
+    else
+        tsc -p "$1" --sourceMap
+    fi
+}
+tsProj . "[Main TypeScript]     "
+tsProj electron "[Electron TypeScript] "
 
 # Build Jekyll content
-jekyll build
+if [ $dev -eq 1 ]; then
+    jekyll serve -H 0.0.0.0 | prefixOutput "[Jekyll]              " &
+else
+    jekyll build
+fi
+
+# Run the development server in the background
+if [ $dev -eq 1 ]; then
+    echo "Press any key to shut down the development server."
+    read -sN1
+    kill $(jobs -p)
+fi
