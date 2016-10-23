983ead9b4688139e66e3fa0f10c40ae83048e571
Zach Deibert <zachdeibert@gmail.com>
2016-10-23 11:39:45 -0500

    Changed dependency installation method

--- compile.sh
+++ compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Update the PATH
+PATH=$PATH:node_modules/.bin/:$(bundle show jekyll)/exe/
+
 # Transpile typescripts
 tsc --sourceMap || true
 tsc -p electron --sourceMap || true
