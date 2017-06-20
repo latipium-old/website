1881b1dd82412df6d8e40bf2ce92ae73cb5364a3
Zach Deibert <zachdeibert@gmail.com>
2017-06-20 13:07:58 -0500

    Added OAuth code for Travis CI

--- ts/oauth.ts
+++ ts/oauth.ts
@@ -1,23 +1,32 @@
 namespace Com.Latipium.Website.OAuth {
     export class Callback {
         public static Main() {
-            let cookies = document.cookie.split("; ");
-            var data;
-            for (var i = 0; i < cookies.length; ++i) {
-                let parts = cookies[i].split("=");
-                if (parts[0] == "OAuthResponse") {
-                    data = JSON.parse(decodeURIComponent(parts[1]));
+            if (location.search == "?travisci") {
+                let expire = new Date();
+                expire.setTime(expire.getTime() + 10 * 60 * 1000);
+                document.cookie = "OAuthResponse=" + encodeURIComponent(JSON.stringify({
+                    "access_token": "true"
+                })) + "; expires=" + expire.toUTCString() + "; path=/;";
+                location.href = "https://api.travis-ci.org/auth/handshake?redirect_uri=https://latipium.com/oauth";
+            } else {
+                let cookies = document.cookie.split("; ");
+                var data;
+                for (var i = 0; i < cookies.length; ++i) {
+                    let parts = cookies[i].split("=");
+                    if (parts[0] == "OAuthResponse") {
+                        data = JSON.parse(decodeURIComponent(parts[1]));
+                    }
                 }
+                let img = document.createElement("img");
+                img.src = "http://localhost:" + data.port + "/" + data.access_token;
+                img.classList.add("hidden");
+                img.addEventListener("load", () => {
+                    $(".oauth-progress").addClass("hidden");
+                    $(".oauth-done").removeClass("hidden");
+                    document.cookie = "OAuthResponse=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                });
+                document.body.appendChild(img);
             }
-            let img = document.createElement("img");
-            img.src = "http://localhost:" + data.port + "/" + data.access_token;
-            img.classList.add("hidden");
-            img.addEventListener("load", () => {
-                $(".oauth-progress").addClass("hidden");
-                $(".oauth-done").removeClass("hidden");
-                document.cookie = "OAuthResponse=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-            });
-            document.body.appendChild(img);
         }
     }
 }
